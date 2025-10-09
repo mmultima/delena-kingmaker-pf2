ef29bb4ac6bd6b3d703771032b7ead687de9cfdb
Mikko Multimäki <mikko.multimaki@kolumbus.fi>
2025-10-09 11:59:46 +0300

    Fix test.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders characters link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Characters/i);
   expect(linkElement).toBeInTheDocument();
 });
