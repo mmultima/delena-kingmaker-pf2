9b21f5a6289b1b3ff0b41c9ab4cb9b74d634ad08
Mikko Multimäki <mikko.multimaki@kolumbus.fi>
2025-10-09 11:57:12 +0300

    Removing unused code.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import logo from './logo.svg';
 import './App.css';
 import CharactersPage from './CharactersPage'; // <-- import the new file
 import Character from './Character'; // <-- import the Character component
@@ -11,7 +10,6 @@ function Home({ helloContent }) {
   const imageEndpoint = `${baseUrl}/image`;
 
   const coverImageUrl = 'https://www.enworld.org/attachments/052620_kingmakercoverart-jpg.122310/';
-  const coverImageEndpoint = `${baseUrl}/cover-image`;  
 
   const [imageUrl, setImageUrl] = useState('');
 
@@ -21,31 +19,15 @@ function Home({ helloContent }) {
 
   return (
     <header className="App-header">
-      {/* <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a> */}
       <div style={{ marginTop: '1em' }}>
         {imageUrl && (
-          // src={imageUrl}
           <img
             src={`${imageEndpoint}/byparam?url=${encodeURIComponent(coverImageUrl)}`}
             alt="Kingmaker"
-            // style={{ marginTop: '2em', maxWidth: '400px', width: '100%' }}
           />
         )}
-        {/* {helloContent} */}
         <br />
         <Link to="/characters">Characters</Link>
-        {/* <br /> */}
         <Link to="/loot">Loot</Link>
         <br />
       </div>
